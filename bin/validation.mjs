db5e3060388ffeecb4c66940764d62df5adf43b3
Georgi Gardev <georgi@gar.dev>
2023-06-21 00:34:57 +0300

    Remove duplicated links

--- bin/validation.mjs
+++ bin/validation.mjs
@@ -1,6 +1,20 @@
-import z from "zod";
-import { error, log } from "./output-utils.mjs";
 import chalk from "chalk";
+import z from "zod";
+import { error, log, warning } from "./output-utils.mjs";
+
+const findDuplicates = (arr) => {
+  let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+  // JS by default uses a crappy string compare.
+  // (we use slice to clone the array so the
+  // original array won't be modified)
+  let results = [];
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
+  }
+  return results;
+};
 
 export const linkSchema = z.object({
   service: z.string().min(1),
@@ -20,7 +34,7 @@ export function validateSubList(subList) {
   log("Validating sub list...");
   const zodValidation = subListSchema.safeParse(subList);
 
-  // Check Zod
+  // Validate Zod schema
   if (!zodValidation.success) {
     const errors = zodValidation.error.issues;
 
@@ -39,29 +53,23 @@ export function validateSubList(subList) {
     process.exit(1);
   }
 
-  // Check name uniqueness
-  const allNames = subList.map((sub) => sub.name.toLowerCase());
-  if (allNames.find((name) => name.startsWith("/"))) {
-    error(`${name}: Subreddit names should not start with a slash`);
+  // Validate link uniqueness
+  const allLinks = subList.flatMap((sub) => sub.links).map((link) => link.url.toLowerCase());
+  const duplicateLinks = findDuplicates(allLinks);
+  if (duplicateLinks.length > 0) {
+    warning("Duplicate links found!");
+    console.log(duplicateLinks);
   }
 
-  const uniqNames = Array.from(new Set(allNames));
-  if (allNames.length !== uniqNames.length) {
-    const findDuplicates = (arr) => {
-      let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
-      // JS by default uses a crappy string compare.
-      // (we use slice to clone the array so the
-      // original array won't be modified)
-      let results = [];
-      for (let i = 0; i < sorted_arr.length - 1; i++) {
-        if (sorted_arr[i + 1] == sorted_arr[i]) {
-          results.push(sorted_arr[i]);
-        }
-      }
-      return results;
-    };
-    const duplicates = findDuplicates(allNames);
-    console.log(duplicates);
+  // Validate name uniqueness
+  const allNames = subList.map((sub) => sub.name.toLowerCase());
+  if (allNames.find((name) => name.startsWith("/") || name.endsWith("/"))) {
+    error(`${name}: Subreddit names should not start or end with a slash`);
+  }
+
+  const duplicateSubs = findDuplicates(allNames);
+  if (duplicateSubs.length > 0) {
+    console.log(duplicateSubs);
     error("Duplicate keys found!");
   }
 
