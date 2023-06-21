7afcbb39ca6e7715532a20764c0fb0e421b0214a
Georgi Gardev <georgi@gar.dev>
2023-06-21 17:49:19 +0300

    Remove console.log

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 
-console.log("client", process.env);
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
