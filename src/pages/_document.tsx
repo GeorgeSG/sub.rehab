6798782cd623b7d2b73da841baf4763ac8cba5f1
Georgi Gardev <georgi@gar.dev>
2023-06-21 17:48:30 +0300

    Enable umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id="8aaffa09-d1cd-4324-b132-cacdf975df7b"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
