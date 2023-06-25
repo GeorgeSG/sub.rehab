8a1b08401a02de06db8a832250ade3f748c61a38
Georgi Gardev <georgi@gar.dev>
2023-06-25 10:06:57 +0300

    Fix raddle color

--- src/data.ts
+++ src/data.ts
@@ -9,7 +9,7 @@ export const SERVICE_COLORS: Record<string, (theme: MantineTheme) => string> = {
   kbin: () => "#D57AD4",
   misc: ({ colors }) => colors.orange[5],
   squabbles: () => "#2581BC",
-  raddle: () => "#4FA736",
+  raddle: () => "#EA3524",
 };
 
 export function useSubredditData() {
